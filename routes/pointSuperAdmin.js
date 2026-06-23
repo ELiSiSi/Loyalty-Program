2f5abb2c6e0457426b1f8b88cb8b2c5859ce6659
Ahmed Rizq <arezk0550@gmail.com>
2026-06-23 17:25:35 +0300

    fix: normalize case for paymentAdmin controller

--- routes/pointSuperAdmin.js
+++ routes/pointSuperAdmin.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-import { protect, restrictTo } from '../middleware/Auth.js';
+import { protect, restrictTo } from '../middleware/auth.js';
 
 import {
   createPoint,
