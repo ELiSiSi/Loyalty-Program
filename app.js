68fe7b28f0872e681950a0f881db4f8b89cae49b
Ahmed Rizq <arezk0550@gmail.com>
2026-06-25 11:28:37 +0300

    fix: normalize case for paymentAdmin controller

--- app.js
+++ app.js
@@ -122,12 +122,7 @@ const options = {
 
 const swaggerSpec = swaggerJsdoc(options);
 
-app.use(
-    '/api-docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec, swaggerUiOptions)
-  );
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 // ═══════════════════════════════════════════════════════════════════════
 //---------------------------   ROUTES    --------------------------------
 // ═══════════════════════════════════════════════════════════════════════
